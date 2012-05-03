11ca734ad5e0d70e3bd7139794b1fee0ab096c1b
lidashuang <ldshuang@gmail.com>
2012-05-03 15:21:01 +0800

    栏目=>数组 中文

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
+#encoding: utf-8
 module ApplicationHelper
+  def nav_menus
+    [
+      ['网站首页',root_path]
+    ]
+  end
+
+  def dashboard_menu
+    [
+      {
+        :name => '管理菜单',
+        :items => [
+          ['教务通知', 'news', admin_news_index_path],
+          ['添加链接', 'links', admin_links_path],
+          ['用户管理', 'users', admin_users_path],
+          ['下载专区', 'document', admin_documents_path]
+        ]
+      }
+    ]
+  end
+
 end
