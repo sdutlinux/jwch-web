2d2ef8bf7cfff6acb5c2c15099d67a09aa0a2ea8
lidashuang <ldshuang@gmail.com>
2012-05-19 21:57:33 +0800

    messages

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,10 +1,33 @@
+#coding: utf-8
 class Admin::MessagesController < ApplicationController
+  layout "admin"
+  before_filter :require_logined
+
   def index
+    @messages = Message.where(:message_channel_id => params[:channel_id])
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(params[:message])
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to [:admin,@message], notice: '创建成功' }
+      els
+        format.html { render action: "new" }
+      end
+    end
   end
 
   def edit
   end
+
 end
--- app/helpers/admin/messages_helper.rb
+++ app/helpers/admin/messages_helper.rb
@@ -1,2 +1,7 @@
 module Admin::MessagesHelper
+
+  def mesage_type_name(mesage_type_id)
+    MessageType.find(mesage_type_id).name
+  end
+
 end
