96cad4060ab387f9c4e815f3dd01c6e5d0abe5f2
lidashuang <ldshuang@gmail.com>
2012-05-20 13:31:17 +0800

    messages

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -20,14 +20,36 @@ class Admin::MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to [:admin,@message], notice: '创建成功' }
-      els
+        format.html { redirect_to admin_messages_path(:channel_id => @message.message_channel_id), notice: '创建成功' }
+      else
         format.html { render action: "new" }
       end
     end
   end
 
   def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:id])
+
+    respond_to do |format|
+      if @message.update_attributes(params[:message])
+        format.html { redirect_to admin_messages_path(:channel_id => @message.message_channel_id), notice: '更新成功' }
+      else
+        format.html { render action: "edit" }
+      end
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+
+    respond_to do |format|
+      format.html {redirect_to admin_messages_path(:channel_id => @message.message_channel_id)}
+    end
   end
 
 end
