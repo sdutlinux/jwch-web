642aa8aca3677528e37af15724c3575dda1445af
lidashuang <ldshuang@gmail.com>
2012-05-21 00:07:59 +0800

    fix validates error_msg

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 JwchWeb::Application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    if instance.error_message.kind_of?(Array)
+         %(#{html_tag}<span style="color:red"> #{instance.error_message.join(',')}</span>).html_safe
+    else
+         %(#{html_tag}<span style="color:red"> #{instance.error_message}</span>)
+    end
+end
