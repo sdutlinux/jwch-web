eac9dfb79763e3cdd55cdc7b56bcaa910b44ab17
lidashuang <ldshuang@gmail.com>
2012-07-27 20:47:02 +0800

    fix index layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,11 @@
 class WelcomeController < ApplicationController
-  layout "welcome"
   def index
-  end
-
-  def main
     @news = News.all
     respond_to do |format|
       format.html
     end
-  end 
+  end
+
   def dep
     @people = Organization.where("category_id=1")
     @organization = Organization.where("category_id=2")
@@ -18,40 +15,4 @@ class WelcomeController < ApplicationController
     end
   end
 
-  def guizcap
-  end
-
-  def zy
-  end
-
-  def kctype 
-  end
-
-  def gongzcap 
-  end
-
-  def caption 
-  end
-
-  def mail 
-  end
-
-  def down 
-  end
-
-  def jiaoxue 
-  end
-
-  def chuangxcap
-  end
-
-  def space 
-  end
-
-  def jx 
-  end
-
-  def show
-
-  end
 end
