f5a46fcb7e88c6b84511026f8ac8995c449c90df
lidashuang <ldshuang@gmail.com>
2012-07-27 21:13:44 +0800

    README  routes setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,9 @@
 #coding=utf-8
 JwchWeb::Application.routes.draw do
   root :to => 'welcome#index'
-  get "main" => 'welcome#main'
-  get "dep" => 'welcome#dep'
-  get "guizcap" => 'welcome#guizcap'
-  get "zy" => 'welcome#zy'
-  get "kctype" => 'welcome#kctype'
-  get "gongzcap" => 'welcome#gongzcap'
-  get "caption" => 'welcome#caption'
-  get "mail" => 'welcome#mail'
-  get "down" => 'welcome#down'
-  get "jiaoxue" => 'welcome#jiaoxue'
-  get "chuangxcap" => 'welcome#chuangxcap'
-  get "space" => 'welcome#space'
-  get "jx" => 'welcome#jx'
-  get "show" => 'welcome#show'
 
-  get "news" => "news#index", :as => :news
-  get "news/:id" => "news#show", :as => :show_news
+  resources :news, :only => [:show]
+
   get "login" => "session#new", :as => :login
   post "login" => "session#create"
   delete "logout" => "session#destroy", :as => :logout
