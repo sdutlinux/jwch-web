cad69f279963d2372147d89ba46591c1f709c9c8
lidashuang <ldshuang@gmail.com>
2012-08-27 22:01:34 +0800

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,19 +6,19 @@ module ApplicationHelper
       {
       :name => '管理菜单',
       :items => [
-          ['机构设置', 'jgsz', admin_organizations_path],
-          ['政策法规', 'zcfg', admin_laws_path],
-          ['规章制度', 'gzzd', admin_rule_types_path],
-          ['课程介绍', 'kcjs', admin_course_types_path],
-          ['工作流程', 'gzlc', admin_workflow_types_path],
-          ['教务通知', 'jwtz', admin_news_index_path],
-          ['下载专区', 'xzzq', admin_documents_path],
-          ['教改立项', 'jglx', admin_education_projects_path],
-          ['教学成果', 'jxcg', admin_teaching_achievements_path],
-          ['创新竞赛', 'cxjs', admin_competition_types_path],
-          ['相关链接', 'xglj', admin_links_path],
-          ['用户管理', 'yhgl', admin_users_path],
-          ['教学简讯', 'jxjx', admin_message_channels_path]
+          ['机构设置', 'jgsz', 'icon-cog', admin_organizations_path],
+          ['政策法规', 'zcfg', 'icon-book', admin_laws_path],
+          ['规章制度', 'gzzd', 'icon-th-list', admin_rule_types_path],
+          ['课程介绍', 'kcjs', 'icon-th-list', admin_course_types_path],
+          ['工作流程', 'gzlc', 'icon-random', admin_workflow_types_path],
+          ['教务通知', 'jwtz', 'icon-file', admin_news_index_path],
+          ['下载专区', 'xzzq', 'icon-download', admin_documents_path],
+          ['教改立项', 'jglx', 'icon-file', admin_education_projects_path],
+          ['教学成果', 'jxcg', 'icon-file', admin_teaching_achievements_path],
+          ['创新竞赛', 'cxjs', 'icon-th-list', admin_competition_types_path],
+          ['相关链接', 'xglj', 'icon-file', admin_links_path],
+          ['用户管理', 'yhgl', 'icon-user', admin_users_path],
+          ['教学简讯', 'jxjx', 'icon-list-alt', admin_message_channels_path]
         ]
     }
     ]
