b6ca2cacb6b576feafc3d6fe43afa94dc18410e1
lidashuang <ldshuang@gmail.com>
2012-09-02 11:20:23 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,27 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "jwch-web"
+
+set :use_sudo, false
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :repository,  "git@github.com:lidashuang/jwch-web.git"
+set :branch, "master"
+set :user, "lidashuang" 
+set :deploy_via, :remote_cache
+default_run_options[:pty] = true  # Must be set for the password prompt
+ssh_options[:forward_agent] = true
+
+set :deploy_to, "/home/group/#{application}"
+server '211.64.30.56', :app, :web, :db, :primary => true
+set :keep_releases, 2
+
+
 
 role :web, "Nginx"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
