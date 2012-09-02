782f210eb5245ad835ef02831c62f7ca9a9817db
lidashuang <ldshuang@gmail.com>
2012-09-02 12:17:18 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,6 @@
+#conding: utf-8
+require 'bundler/capistrano'                #添加之后部署时会调用bundle install， 如果不需要就可以注释掉
+
 set :application, "jwch-web"
 
 set :use_sudo, false
@@ -11,13 +14,16 @@ default_run_options[:pty] = true  # Must be set for the password prompt
 ssh_options[:forward_agent] = true
 
 set :deploy_to, "/home/group/#{application}"
-server '211.64.30.55', :app, :web, :db, :primary => true
+set :current_public,   "/home/group/#{application}/current/public"
+set :shared_path,      "/home/group/#{application}/shared"
+
+
 set :keep_releases, 2
 
 set :bundle_without, [:development,:test] 
 
-
-role :web, "Nginx"                          # Your HTTP server, Apache/etc
+server '211.64.30.55', :app, :web, :db, :primary => true
+# role :web, "Nginx"                          # Your HTTP server, Apache/etc
 # role :app, "your app-server here"                          # This may be the same as your `Web` server
 # role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
