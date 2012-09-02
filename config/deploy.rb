2f75303910ea2e5fa7269dd926a955818ac0bd29
lidashuang <ldshuang@gmail.com>
2012-09-02 12:27:18 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,30 @@
 #conding: utf-8
 require 'bundler/capistrano'                #添加之后部署时会调用bundle install， 如果不需要就可以注释掉
 
-set :application, "jwch-web"
-
 set :use_sudo, false
+set :rvm_type, :system 
+require 'rvm/capistrano'
 
-set :scm, :git
-set :repository,  "git@github.com:lidashuang/jwch-web.git"
-set :branch, "master"
-set :user, "lidashuang" 
-set :deploy_via, :remote_cache
+set :application, "jwch-web"
 default_run_options[:pty] = true  # Must be set for the password prompt
+
+set :repository,  "git@github.com:lidashuang/jwch-web.git"
+set :deploy_via, :remote_cache
+set :user, "lidashuang" 
+set :scm, :git
+set :branch, "master"
 ssh_options[:forward_agent] = true
 
 set :deploy_to, "/home/group/#{application}"
 set :current_public,   "/home/group/#{application}/current/public"
 set :shared_path,      "/home/group/#{application}/shared"
 
-
+server '211.64.30.55', :app, :web, :db, :primary => true
+set :rails_env, "production"
+set :rvm_ruby_string, 'ruby-1.9.3-p194@jwch-web'
 set :keep_releases, 2
-
 set :bundle_without, [:development,:test] 
 
-server '211.64.30.55', :app, :web, :db, :primary => true
 # role :web, "Nginx"                          # Your HTTP server, Apache/etc
 # role :app, "your app-server here"                          # This may be the same as your `Web` server
 # role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
@@ -37,10 +39,13 @@ server '211.64.30.55', :app, :web, :db, :primary => true
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :task_name  do
+  end
+end
