972bf354fa18da94e6c84d071d175429dbda5534
lidashuang <ldshuang@gmail.com>
2012-10-09 10:07:05 -0400

    capi

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,10 +8,13 @@ set :rvm_ruby_string, 'ruby-1.9.3-p194@jwch-web'
 set :deploy_to, "/home/group/#{application}"
 
 #  2012-10-01
-#after "bundle:install", "deploy:cp_config_file"
+#after "bundle:install", "deploy:config_file"
 #after "deploy:config_file", "deploy:create_database"
 #after "deploy:create_database", "deploy:migrate"
 #after "deploy:migrate", "deploy:init"
 #after "deploy:init", "deploy:import_data"
 #after "deploy:create_symlink", "deploy:cleanup"
 #
+
+after "bundle:install", "deploy:cp_config_file", "deploy:migrate"
+after "deploy:create_symlink", "deploy:cleanup"
