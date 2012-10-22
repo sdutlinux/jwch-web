9d8e7ee79f17dfec0b33f8ca0e88c9290a3177f5
lidashuang <ldshuang@gmail.com>
2012-10-22 10:18:47 -0400

    fix

--- app/controllers/education_projects_controller.rb
+++ app/controllers/education_projects_controller.rb
@@ -1,11 +1,7 @@
 class EducationProjectsController < ApplicationController
-  layout "application"
   def index
-    @education_projects_1 = EducationProject.where( :rank => "nation" )
-    @education_projects_2 = EducationProject.where( :rank => "province" )
-    @education_projects_3 = EducationProject.where( :rank => "school" )
 
-    @education_projects = [ @education_projects_1, @education_projects_2, @education_projects_3 ]
+    @education_projects = EducationProject.all
 
     respond_to do |format|
       format.html
