cea14366a07bdaff14ef344fb7ac4f3abdf1321e
liuzhe <liuzhe0223@gmail.com>
2012-10-23 16:25:55 +0800

    bak

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,6 +1,11 @@
 class MessageController < ApplicationController
   layout 'message_channel'
   def show
+    @message = Message.find( params[:id] )
+
+    respond_to do |format|
+      format.html
+    end
     
   end
 end
