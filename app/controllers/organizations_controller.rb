818b6f14b3e809f6ea18b68c42232281bb3e9648
liuzhe <liuzhe0223@gmail.com>
2012-10-25 17:15:45 +0800

    修改领导顺序

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,6 +1,6 @@
 class OrganizationsController < ApplicationController
   def index 
-    @people = Organization.where("category_id=1")
+    @people = Organization.where("category_id=1").order('id desc')
     @organization = Organization.where("category_id=2")
 
     respond_to do |format|
