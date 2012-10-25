6525b8edacbd5aba2fd7636b250fbb76c362eeb0
liuzhe <liuzhe0223@gmail.com>
2012-10-25 19:56:25 +0800

    fix index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @news = News.order('created_at DESC').limit(10)
-    @message_channels = MessageChannel.order('created_at DESC').limit(5)
+    @news = News.order('created_at DESC').limit(11)
+    @message_channels = MessageChannel.order('created_at DESC').limit(6)
 
     respond_to do |format|
       format.html
