93dba2383f43af1016d572386f32cb719de4d5ad
liuzhe <liuzhe0223@gmail.com>
2012-10-28 19:11:44 +0800

    add competitions index

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,7 +1,33 @@
+#coding: utf-8
 class CompetitionsController < ApplicationController
+  before_filter :find_competition_type
+
   def index
+    @competitions = @competition_type.competitions.paginate :page => params[:page], :order => 'created_at desc',
+      :per_page => 10
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   def show
+    @competition = @competition_type.competitions.find(params[:id])
+
+    respond_to do |format|
+      format.html
+    end
   end
+
+
+  private
+
+  def find_competition_type
+    @competition_type = Category.find(params[:category_id])
+  end
+
+  def set_section_key
+    @section_key = 'cxjs'
+  end
+
 end
